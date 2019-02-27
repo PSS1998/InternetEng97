cac1a7a4c24152bdd1875014f00945091d76c8db
PSS <p_sadri_s@yahoo.com>
2019-02-27 19:08:20 +0330

    IU-2018.2.4 <PSS@DESKTOP-UGV9614 Overwrite remote https://github.com/PSS1998/InternetEng97.git to local

--- src/main/java/server/IPage.java
+++
@@ -1,9 +0,0 @@
-package server;
-
-import java.io.IOException;
-
-import com.sun.net.httpserver.HttpExchange;
-
-public interface IPage {
-	public void HandleRequest(HttpExchange httpExchange) throws IOException;
-}
